947613d024efeffe75c665d7dbae68ef8bf180ee
emir93795 <emir93795@uoc.edu>
2014-10-13 09:19:51 +0200

    Update MainScript.sh

--- MainScript.sh
+++ MainScript.sh
@@ -5,7 +5,7 @@ yum update -y
 
 #InstallingBasicPackages
 #yum groupinstall -y "Web Server" "MySQL Database" 
-sudo yum install -y php55.x86_64 mysql55-server.x86_64 php55-mysqlnd.x86_64 unzip
+sudo yum install -y php55.x86_64 mysql55-server.x86_64 php55-mysqlnd.x86_64 php55-mysqlnd.x86_64 unzip  php55-gd.x86_64 
 
 #ApacheHttpAndMySQLServicesStart
 service httpd start
@@ -25,13 +25,33 @@ find /var/www -type f -exec chmod 0664 {} +
 echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
 
 #InstallingGIT
-yum install -y git
+#yum install -y git
 
 #InstallingLTI(Moodle)
 cd /var/www/html/
 wget  --no-check-certificate https://github.com/moodle/moodle/archive/MOODLE_27_STABLE.zip 
-unzip MOODLE_27_STABLE.zip 
+unzip MOODLE_27_STABLE.zip
+mv moodle-MOODLE_27_STABLE moodle
+rm -f MOODLE_27_STABLE.zip
 chown -R ec2-user .
+
+#CreatingMoodleDataFolderAndGivingPermissions
+cd /var/www/
+mkdir moodledata
+chown -R apache /var/www/moodledata
+chown -R apache /var/www/html/moodle
+
+#CreatingMoodleDataBase
+cd html/moodle
+wget  --no-check-certificate https://github.com/emir93795/LTISetUp/archive/master.zip
+unzip master.zip
+rm -f master.zip
+cd LTISetUp-master
+mysql < MoodleDatabaseCreation.sql
+cd /var/www/html/moodle
+
+sudo -u apache /usr/bin/php admin/cli/install.php --non-interactive --agree-license --wwwroot=http://54.207.102.15/moodle/ --dataroot=/var/www/moodledata --dbtype=mysqli --dbuser=moodle --dbpass=secretpassword --fullname=Moodle --shortname=Mood --adminpass=secretpassword
+
 #git clone git://git.moodle.org/moodle.git
 #cd moodle
 #git branch -a
