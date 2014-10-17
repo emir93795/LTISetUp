f4b6d30f2adcfc8ca35c0dbd16d65ee666e65c42
emir93795 <emir93795@uoc.edu>
2014-10-17 10:09:49 +0200

    Update LAMPMoodleScript.sh

--- LAMPMoodleScript.sh
+++ LAMPMoodleScript.sh
@@ -1,54 +1,52 @@
 #!/bin/bash
-sudo su 
 #Updating
-yum update -y
+sudo yum update -y
 
 #InstallingBasicPackages
 #yum groupinstall -y "Web Server" "MySQL Database" 
 sudo yum install -y php55.x86_64 mysql55-server.x86_64 php55-mysqlnd.x86_64 php55-mysqlnd.x86_64 unzip  php55-gd.x86_64 
 
 #ApacheHttpAndMySQLServicesStart
-service httpd start
-chkconfig httpd on
-service mysqld start
-chkconfig mysqld on
+sudo service httpd start
+sudo chkconfig httpd on
+sudo service mysqld start
+sudo chkconfig mysqld on
 
 #EditingUserPermissionAndGroups
-groupadd www
-usermod -a -G www ec2-user
-chown -R root:www /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} +
-find /var/www -type f -exec chmod 0664 {} +
+sudo groupadd www
+sudo usermod -a -G www ec2-user
+sudo chown -R root:www /var/www
+sudo chmod 2775 /var/www
+sudo find /var/www -type d -exec chmod 2775 {} +
+sudo find /var/www -type f -exec chmod 0664 {} +
 
 #CreatingBasicPhpInfoWeb
-echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+sudo echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
 
 #InstallingGIT
 #yum install -y git
 
 #InstallingLTI(Moodle)
 cd /var/www/html/
-wget  --no-check-certificate https://github.com/moodle/moodle/archive/MOODLE_27_STABLE.zip 
-unzip MOODLE_27_STABLE.zip
-mv moodle-MOODLE_27_STABLE moodle
-rm -f MOODLE_27_STABLE.zip
-chown -R ec2-user .
+sudo wget  --no-check-certificate https://github.com/moodle/moodle/archive/MOODLE_27_STABLE.zip 
+sudo unzip MOODLE_27_STABLE.zip
+sudo mv moodle-MOODLE_27_STABLE moodle
+sudo rm -f MOODLE_27_STABLE.zip
+sudo chown -R ec2-user .
 
 #CreatingMoodleDataFolderAndGivingPermissions
 cd /var/www/
-mkdir moodledata
-chown -R apache /var/www/moodledata
-chown -R apache /var/www/html/moodle
+sudo mkdir moodledata
+sudo chown -R apache /var/www/moodledata
+sudo chown -R apache /var/www/html/moodle
 
 #CreatingMoodleDataBase
 cd html/moodle
-wget  --no-check-certificate https://github.com/emir93795/LTISetUp/archive/master.zip
-unzip master.zip
-rm -f master.zip
+sudo wget  --no-check-certificate https://github.com/emir93795/LTISetUp/archive/master.zip
+sudo unzip master.zip
+sudo rm -f master.zip
 cd LTISetUp-master
-mysql < MoodleDatabaseCreation.sql
-cd /var/www/html/moodle
+sudo mysql < MoodleDatabaseCreation.sql
 
 #Getting instance public IP
 publicIp=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
@@ -56,7 +54,7 @@ publicIp=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 #InstallingMoodle
 cd /var/www/html/moodle
 sudo -u apache /usr/bin/php admin/cli/install.php --non-interactive --agree-license --wwwroot=http://$publicIp/moodle/ --dataroot=/var/www/moodledata --dbtype=mysqli --dbuser=moodle --dbpass=secretpassword --fullname=Moodle --shortname=Mood --adminpass=secretpassword
-exit
+
 
 #git clone git://git.moodle.org/moodle.git
 #cd moodle
